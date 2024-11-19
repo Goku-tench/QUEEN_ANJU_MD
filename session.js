7c4cf2d5ea7f97b6e704bef40a984a1644740c83
Goku-tench <godblessisdori9@gmail.com>
2024-11-20 01:23:47 +0300

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=GVEhDY7Y#H8BSf8mt1ji6yQdNs4XPlTltRfsSrRYI5lE2frqqOAs",
 };
